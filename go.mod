54f45888e6800eb6414ed66e35216829a6d1762c
Kirill Sysoev <k.sysoev@me.com>
2026-06-18 22:49:28 +0700

    Adds basic implementation for BadgerDB

--- go.mod
+++ go.mod
@@ -13,8 +13,20 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgraph-io/badger/v4 v4.9.2 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/flatbuffers v25.2.10+incompatible // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ storage/badger/badger.go
@@ -0,0 +1,112 @@
+package badger
+
+import (
+	"context"
+	"errors"
+	"math"
+	"time"
+
+	"github.com/dgraph-io/badger/v4"
+)
+
+type Storage struct {
+	client *badger.DB
+}
+
+func New(client *badger.DB) *Storage {
+	return &Storage{
+		client: client,
+	}
+}
+
+func (s *Storage) Get(_ context.Context, key string) ([]byte, error) {
+	var val []byte
+
+	err := s.client.View(func(txn *badger.Txn) error {
+		v, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		err = v.Value(func(valBytes []byte) error {
+			val = append([]byte{}, valBytes...)
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
+}
+
+func (s *Storage) Set(_ context.Context, key string, value []byte, ttl time.Duration) error {
+	return s.client.Update(func(txn *badger.Txn) error {
+		entity := badger.NewEntry([]byte(key), value)
+		if ttl > 0 {
+			entity.WithTTL(ttl)
+		}
+
+		return txn.SetEntry(entity)
+	})
+}
+
+func (s *Storage) Delete(_ context.Context, key string) (bool, error) {
+	err := s.client.Update(func(txn *badger.Txn) error {
+		return txn.Delete([]byte(key))
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (s *Storage) GetWithTTL(_ context.Context, key string) ([]byte, time.Duration, error) {
+	var (
+		val []byte
+		ttl time.Duration
+	)
+
+	err := s.client.View(func(txn *badger.Txn) error {
+		v, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		err = v.Value(func(valBytes []byte) error {
+			val = append([]byte{}, valBytes...)
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		expiresAt := v.ExpiresAt()
+		if expiresAt == 0 {
+			return nil
+		} else if expiresAt > math.MaxInt64 {
+			return errors.New("expiresAt value exceeds maximum int64 value")
+		}
+
+		expiresAtTime := time.Unix(int64(expiresAt), 0)
+
+		remaining := time.Until(expiresAtTime)
+		if remaining <= 0 {
+			return nil
+		}
+
+		ttl = remaining
+
+		return nil
+	})
+
+	return val, ttl, err
+}
